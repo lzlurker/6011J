b9bdfa831e5c41429a89b39e8349955acafbc718
lzlurker <31750946+lzlurker@users.noreply.github.com>
2018-08-10 13:05:29 -0400

    user

--- admin/userorder_subscribe_do.php
+++ admin/userorder_subscribe_do.php
@@ -28,16 +28,16 @@
 			$result=mysql_query($sql);
 			$row=mysql_fetch_assoc($result);
 			if(!$row){
-				alertInfo('您要删除的订单不存在','userorder.php?key='.$key.$url,0);
+				alertInfo('Order not exist','userorder.php?key='.$key.$url,0);
 			}else{
 				$sql2="delete from qiyu_order where order_id=".$id;
 				if(mysql_query($sql2)){
 					$sql3="delete from qiyu_orderchange where orderchange_order='".$row['order_id2']."'";
 					if(mysql_query($sql3)){
-						alertInfo('删除成功','',1);
+						alertInfo('Deleted','',1);
 					}
 				}else{
-					alertInfo('删除失败，原因SQL出现异常','',1);
+					alertInfo('Failed, check SQL','',1);
 				}
 			}
 			break;
@@ -45,7 +45,7 @@
 			$id_list=$_POST["id_list"];
 			$key=sqlReplace(trim($_GET['key']));
 		    if(empty($id_list)){
-				alertInfo('请选择删除项!','userorder.php?key='.$key,0);
+				alertInfo('Selete to delete!','userorder.php?key='.$key,0);
 			}else{
 				foreach($id_list as $val){
 					$sql="select * from qiyu_order where order_id=".$val."";
@@ -58,14 +58,14 @@
 							$sql3="delete from qiyu_orderchange where orderchange_order='".$row['order_id2']."'";
 							mysql_query($sql3);
 						}else{
-							alertInfo('删除失败！原因：SQL删除失败。',"",1);
+							alertInfo('Failed, check SQL。',"",1);
 						}
 					}else{
-						alertInfo('删除失败！原因：不存在',"",1);
+						alertInfo('Failed, not exist',"",1);
 						exit;
 					}
 				}
-				alertInfo('删除成功','',1);
+				alertInfo('Deleted','',1);
 			}
 			break;
 		case 'qx':
@@ -77,7 +77,7 @@
 			$row=mysql_fetch_assoc($result);
 			$order_status=$row['order_status'];
 			if(!$row){
-				alertInfo('订单不存在','',1);
+				alertInfo('Order not exist','',1);
 			}else{
 				if ($order_status!='2'){
 					$order=$row['order_id2'];
@@ -88,15 +88,15 @@
 					}
 					if(mysql_query($sql2)){
 						//添加订单记录
-						$orderContent="<span class='greenbg redbg'><span><span>取消订单</span></span></span>";
-						$orderContent.="您的订单已取消，给您带来的不便请谅解，我们会更好的为您服务。";
+						$orderContent="<span class='greenbg redbg'><span><span>Cancel order</span></span></span>";
+						$orderContent.="Canceled。";
 						addOrderType($order,HTMLEncode($orderContent));
-						alertInfo('取消成功','',1);
+						alertInfo('Canceled','',1);
 					}else{
-						alertInfo('取消失败，原因SQL出现异常','',1);
+						alertInfo('Failed, check SQL','',1);
 					}
 				}else{
-					alertInfo('取消成功','',1);
+					alertInfo('Success','',1);
 				}
 			}
 			break;
@@ -108,18 +108,18 @@
 			$result=mysql_query($sql);
 			$row=mysql_fetch_assoc($result);
 			if(!$row){
-				alertInfo('订单不存在','userorder.php?key='.$key.$url,0);
+				alertInfo('Order not exist','userorder.php?key='.$key.$url,0);
 			}else{
 				$order=$row['order_id2'];
 				$sql2="update qiyu_order set order_status='1'  where order_id=".$id;
 				if(mysql_query($sql2)){
 					//添加订单记录
 					$orderContent="<span class='greenbg'><span><span>我们正在下单</span></span></span>";
-					$orderContent.="亲，大厨正在努力烹制美味的食物，请耐心等待！";
+					$orderContent.="Cooking！";
 					addOrderType($order,HTMLEncode($orderContent));
-					alertInfo('确定成功','',1);
+					alertInfo('Success','',1);
 				}else{
-					alertInfo('确定失败，原因SQL出现异常','',1);
+					alertInfo('Failed, check SQL','',1);
 				}
 			}
 			break;	
@@ -131,18 +131,18 @@
 			$result=mysql_query($sql);
 			$row=mysql_fetch_assoc($result);
 			if(!$row){
-				alertInfo('订单不存在','',1);
+				alertInfo('Order not exist','',1);
 			}else{
 				$order=$row['order_id2'];
 				$sql2="update qiyu_order set order_status='4'  where order_id=".$id." and order_status='1'";
 				if(mysql_query($sql2)){
 					//添加订单记录
 					$orderContent="<span class='greenbg'><span><span>订单已完成</span></span></span>";
-					$orderContent.="亲，享受美味的时候，别忘了继续光顾".$SHOP_NAME."哦，我们将更好的为您服务。";
+					$orderContent.="Enjoy".$SHOP_NAME."Thanks。";
 					addOrderType($order,HTMLEncode($orderContent));
-					alertInfo('设置订单为已完成成功','',1);
+					alertInfo('Setup success','',1);
 				}else{
-					alertInfo('取消失败，原因SQL出现异常','',1);
+					alertInfo('Failed, check SQL','',1);
 				}
 			}
 			break;
